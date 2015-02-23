a127c1dd8b4828f652a0cdb51668ab8b0ec297bf
MiniWitch3 <med94man@yahoo.com>
2015-02-23 14:44:59 -0800

    Beginning stuff for Pluto

---
+++ Pluto/Count Pluto.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var Pluto = new Image();
+Pluto.src = "Pluto.png"
+
+
+function collide(){
+	
+	//if Phil even touches him, death
+	
+}
+

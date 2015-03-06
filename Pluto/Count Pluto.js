0952ce7bcbdb1f6359293daa3b3e19f563a25610
MiniWitch3 <med94man@yahoo.com>
2015-03-05 21:39:41 -0800

    Updated Phases

--- Pluto/Count Pluto.js
+++ Pluto/Count Pluto.js
@@ -1,57 +1,87 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-var Pluto = new Image();
-Pluto.src = "Pluto.png";
+var Pluto = {img: null, x: 300, y: 0,
+             width: 200, height: canvas.height, sprite: "Pluto.png"};
+Pluto.img = new Image();
+Pluto.img.src = Pluto.sprite;
 
-var Charon = new Image();
-Charon.src = "Charon.png";
-
-var Rocket = new Image();
-Rocket.src = "Rocket.png";
 
 var gravity = 0;
 
 
 function collide(){
 	
-    var Px1 = Pluto.x + Pluto.width;
-    var Py1 = Pluto.y + Pluto.height;
-    var Px2 = Phil.x + Phil.width;
-    var Py2 = Phil.y + Phil.height;
-	
-    if(Pluto.x >= Phil.x && Pluto.x <= Px2 && Pluto.y >= Phil.y && Pluto.y <= Py2){
-          health -= 100;
-        
-        
-    }else if(Pluto.x >= Phil.x && Pluto.x <= Px2 && Py1 >= Phil.y && Py1 <= Py2){
-          health -= 100;
-          
-          
-    }else if(Px1 >= Phil.x && Px1 <= Px2 && Pluto.y >= Phil.y && Pluto.y <= Py2){
-          health -= 100;
-          
-          
-    }else if(Px1 >= Phil.x && Px1 <= Px2 && Py1 >= Phil.y && Py1 <= Py2){
-          health -= 100;
-          
+    if(AABB(Pluto.x, player.x, Pluto.y, player.y, Pluto.width, 
+	        player.width, Pluto.height, player.height) == true){
+	    console.log("collision!");
+	    health -= 100;
     }
-	
 }
 
 function phase1(){
 	
 	//toss Charon at Phil
+	var Charon = {img: null, x: 500, y: 300, rotation: 0,
+             width: 200, height: 200, sprite: "Charon.png"};
+    Charon.img = new Image();
+    Charon.img.src = Charon.sprite;
+    
+    if(AABB(Charon.x, player.x, Charon.y, player.y, Charon.width, 
+	        player.width, Charon.height, player.height) == true){
+	    console.log("collision!");
+	    health -= 100;
+    }
+    
+    setInterval(function(){
+        Charon.x -= 5;
+        Charon.rotation += (10 * Math.PI/180);
+    }, 5);
 }
 
 function phase2(){
 	
 	//call ten meteors
+	var rock = new meteor(10, 10, 10 ,10, "Meteor.png");
+	
+	for(var i = 0; i < rocks.length; i++){
+        var obj = rocks[i];
+        ctx.drawImage(obj.img, obj.x, obj.y, obj.width, obj.height);
+    }
 }
 
 function phase3(){
 	
 	//fire rockets at Phil
+	
+	setInterval(function(){
+	    var Rocket = {img: null, x: 500, y: 350, rotation: 0,
+	                  width: 300, height ; 100, sprite: "Rocket.png"};
+	    Rocket.img = new Image();
+        Rocket.img.src = Rocket.sprite;
+    
+        if(AABB(Rocket.x, player.x, Rocket.y, player.y, Rocket.width, 
+    	        player.width, Rocket.height, player.height) == true){
+	        console.log("collision!");
+	        if (shield > 0) shield -= 40;
+	        else if(shield <= 0) health -= 40;
+        }
+    
+        var xRoc = player.x - Rocket.x;
+        var yRoc = player.y - Rocket.y;
+    
+        var Rangle = Math.atan2(yRoc, xRoc);
+    
+        Rocket.rotation = Rangle;
+    
+        var xvelRoc = Math.cos(Rangle) * 5;
+        var yvelRoc = Math.sin(Rangle) * 5;
+    
+        setInterval(function(){
+            Rocket.x += xvelRoc;
+            Rocket.y += yvelRoc;
+        }, 5);
+    }, 3000);
 }
 
 function phase4(){
